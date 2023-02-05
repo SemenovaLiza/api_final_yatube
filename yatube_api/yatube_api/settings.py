0ed11ae8e7340267516fdd0d05396558031d251c
Семенова Елизавета <thealeeeaaa23@gmail.com>
2023-02-05 22:45:51 +0400

    1

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -18,8 +18,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'djoser',
     'django_filters',
+    'djoser',
     'api',
     'posts',
 ]
